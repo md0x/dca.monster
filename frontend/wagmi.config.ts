1a031141384008d159166dd13738a33cb2c36afd
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2023-07-22 14:55:19 +0200

    feat: wagmi generate
    Signed-off-by: Pablo Maldonado <pablomaldonadoturci@gmail.com>

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@wagmi/cli'
 import { react } from '@wagmi/cli/plugins'
 import { erc20ABI } from 'wagmi'
+import { ICartesiDApp__factory, IERC20Portal__factory, IInputBox__factory } from '@cartesi/rollups'
 
 export default defineConfig({
   out: 'lib/generated/blockchain.ts',
@@ -8,6 +9,18 @@ export default defineConfig({
     {
       name: 'erc20',
       abi: erc20ABI,
+    },
+    {
+      name: 'cartesiDapp',
+      abi: ICartesiDApp__factory.abi,
+    },
+    {
+      name: 'cartesiInputBox',
+      abi: IInputBox__factory.abi,
+    },
+    {
+      name: 'cartesiErc20Portal',
+      abi: IERC20Portal__factory.abi,
     },
   ],
   plugins: [react()],
--- readme.md
+++ readme.md
@@ -20,3 +20,14 @@
 ## Solution
 
 ## Implementation
+- Dapp/backend:
+  - Starting from the [cartesi-dapp rollups-examples custom dapp starter](https://github.com/cartesi/rollups-examples/tree/main/custom-dapps)
+    - Switched to using RISC-V base Docker images to install python dependencies
+    - Dapp ready to be run in "prod" mode
+- Frontend:
+  - Starting from the [template-web3-app](https://github.com/turbo-eth/template-web3-app) by [TurboETH](https://www.turboeth.xyz/) that uses:
+    - Next JS
+    - Tailwind CSS
+    - Web3 React
+    - Wagmi
+    - RainbowKit
